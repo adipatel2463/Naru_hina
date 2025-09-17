97849ebeaef97065bf35c25efd3990c8436598fd
adipatel2463 <adipatel2463@gmail.com>
2025-09-17 10:32:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -64,9 +64,9 @@ LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '100'))
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-100'))
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
 
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/moviehub4u_update")
-TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/moviehub4u_update")
-TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/moviehub4u_update")
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/moviemagic_24")
+TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/moviemagic_24")
+TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/moviemagic_24")
 
 SHORTENER_API = environ.get("SHORTENER_API", "")
 SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "")
